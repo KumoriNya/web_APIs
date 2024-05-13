63eb8a7a08e2647227633bc7d6ea79635a78ab01
Abigail <z5314102@ad.unsw.edu.au>
2024-05-13 18:17:55 +1000

    change return val

--- src/backend_app.py
+++ src/backend_app.py
@@ -11,13 +11,13 @@ cors = CORS(app, resources={r"/*": {"origins":"*"}})
 @app.route("/", methods=["GET"])
 def hello():
     print("Successful Get Test")
-    return "Hello world", 200
+    return "Hi world", 200
 
 @app.route("/test", methods=["GET"])
 def test_get():
     token = request.authorization.token
     print("Successful Get Test")
-    return "Hello world", 200
+    return "Hey world", 200
 
 @app.route("/test", methods=["POST"])
 def test_post():
